904b85bc6e591b03faed14e3d85a3ceeb98a515b
D069379 <oemer.sagsoez@sap.com>
2026-06-02 00:17:30 +0200

    feat: scaffold data files and config template

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// This file is generated at deploy time by GitHub Actions.
+// Do NOT commit real values here.
+const CONFIG = {
+  owner: 'x-talks',
+  repo: 'Park_Management',
+  branch: 'main',
+  pat: '__PAT_PLACEHOLDER__'
+};
