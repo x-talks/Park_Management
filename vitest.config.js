458a623af9a63163d0cc53520e8b117d52d98935
D069379 <oemer.sagsoez@sap.com>
2026-06-30 23:55:16 +0200

    chore: add coverage script, include .spec.js in vitest glob

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['tests/unit/**/*.test.js'],
+    include: ['tests/unit/**/*.{test,spec}.js'],
     environment: 'jsdom',
     globals: true,
     coverage: {
